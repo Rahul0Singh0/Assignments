cb45184f3548945d6a1f5ba62b34bbca1372bf26
rahul0singh0 <thesincererahul@gmail.com>
2024-02-27 20:22:07 +0530

    Program1 Update

--- Assignment 56/Pro1.cpp
+++ Assignment 56/Pro1.cpp
@@ -1,3 +1,14 @@
+/*
+Given a linked list and a key ‘X‘ in, the task is to check if X is present in the linked list
+or not.
+Examples:
+Input: 14->21->11->30->10, X = 14
+Output: Yes
+Explanation: 14 is present in the linked list.
+Input: 6->21->17->30->10->8, X = 13
+Output: No
+
+*/
 #include <bits/stdc++.h>
 using namespace std;
 class Node
@@ -78,15 +89,16 @@ public:
         cout << endl;
     }
 };
-int findNode(Node* head, int k)
+string findNode(Node* head, int data)
 {
-    Node *temp = head;
-    while (temp != NULL)
-    {
-        if(temp->val == k)
-            return 1;
+    Node* temp = head;
+    while (temp != NULL) {
+        if (temp->val == data) {
+            return "Yes";
+        }
+        temp = temp->next;
     }
-    return 0;
+    return "No";
 }
 int main()
 {
