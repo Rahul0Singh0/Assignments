83d4012baadb73ebb055dc2494704db01055a69f
rahul0singh0 <thesincererahul@gmail.com>
2024-02-27 18:38:43 +0530

    Program 1

---
+++ Assignment 56/Pro1.cpp
@@ -0,0 +1,8 @@
+// Given a linked list and a key ‘X‘ in, the task is to check if X is present in the linked list
+// or not.
+// Examples:
+// Input: 14->21->11->30->10, X = 14
+// Output: Yes
+// Explanation: 14 is present in the linked list.
+// Input: 6->21->17->30->10->8, X = 13
+// Output: No
